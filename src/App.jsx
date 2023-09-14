7f5ff442f72856695746202e5eb8e1630279c3ef
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-09-14 19:58:54 +0600

    added header section and course data: implement a header section in the ui and creat some fake data type of array of objects, course information and covert to JSON data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className=" text-3xl font-bold">Course Registration</h1>
+      <Header></Header>
     </>
   );
 }
